73b76bca752913cc4500de539717473e1d388032
clara <10088152+claraj@users.noreply.github.com>
2021-09-16 15:53:42 -0500

    tidying

--- tetris.py
+++ tetris.py
@@ -1,6 +1,3 @@
-from tkinter import * 
-import random
-
 """
 
 Tkinter - no PyGame, no classes, not much math 
@@ -17,29 +14,35 @@ more things that could be added
 * bug - checking for game over - if new piece doesn't fit, it is not drawn and gap shown at top of screen
 """
 
+from tkinter import Tk, Canvas
+import random
+
+
 root = Tk()
+
 
 speed = 500
 
-grid_line_color = 'gray'
-background = 'black'
+GRID_LINE_COLOR = 'gray'
+BACKGROUND_COLOR = 'black'
 
-canvas_width = 240 
-canvas_height = 300
+CANVAS_WIDTH = 240 
+CANVAS_HEIGHT = 300
 
-square_side = 20
-squares_width = int(canvas_width / square_side)
-squares_height = int(canvas_height / square_side)
+SQUARE_SIDE = 20
+SQUARES_WIDTH = int(CANVAS_WIDTH / SQUARE_SIDE)
+SQUARES_HEIGHT = int(CANVAS_HEIGHT / SQUARE_SIDE)
 
-w = Canvas(root, width=canvas_width, height=canvas_height)
+w = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 w.pack() 
 
 grid: list 
 
+
 pieces = {
     'square': {  
         'color': 'red', 'orientations': 
-            [ '**\n**' ] 
+            ['**\n**'] 
     },
     't': { 
         'color': 'green', 'orientations': 
@@ -47,7 +50,7 @@ pieces = {
     },
     'line': { 
         'color': 'yellow', 'orientations': 
-            ['*\n*\n*\n*', '****', ] 
+            ['*\n*\n*\n*', '****'] 
     },
     'left-l': { 
         'color': 'blue', 'orientations': 
@@ -55,96 +58,102 @@ pieces = {
     },
     'right-l': { 
         'color': 'orange', 'orientations': 
-            [ ' *\n *\n**', '***\n  *', '**\n* \n* ',  '  *\n***'] 
+            [' *\n *\n**', '***\n  *', '**\n* \n* ',  '  *\n***'] 
     },
     's': { 
         'color': 'lawngreen', 'orientations': 
-        [' **\n** ', '* \n**\n *'] 
+            [' **\n** ', '* \n**\n *'] 
     },
-    'z': { 'color': 'purple', 'orientations': 
-        [ '** \n **', ' *\n**\n* '] 
+    'z': {
+        'color': 'purple', 'orientations': 
+            ['** \n **', ' *\n**\n* '] 
     },
 }
 
-active_piece_info: dict
-active_piece_orientation_index: int
-active_piece_orientation: str
-active_piece_col: int
-active_piece_row: int
+ACTIVE_PIECE_INFO: dict
+ACTIVE_PIECE_ORIENTATION_INDEX: int
+ACTIVE_PIECE_ORIENTATION: str
+ACTIVE_PIECE_COL: int
+ACTIVE_PIECE_ROW: int
 
 def initialize_game():
+    """ Set up grid, and create first piece """
     global grid
     grid = []
-    for x in range(squares_height):
+    for x in range(SQUARES_HEIGHT):
         grid.append(make_empty_row())
     start_new_piece()
 
 
 def make_empty_row():
     line = []
-    for y in range(squares_width):
+    for y in range(SQUARES_WIDTH):
         line.append('')
     return line 
 
 
 def start_new_piece():
-    global active_piece_row, active_piece_col, active_piece_info
-    global active_piece_orientation, active_piece_orientation_index 
+    global ACTIVE_PIECE_ROW, ACTIVE_PIECE_COL, ACTIVE_PIECE_INFO
+    global ACTIVE_PIECE_ORIENTATION, ACTIVE_PIECE_ORIENTATION_INDEX 
 
     active_piece_name = random.choice(list(pieces.keys()))
-    active_piece_info = pieces[active_piece_name]
-    active_piece_orientation_index = 0
-    active_piece_orientation = active_piece_info['orientations'][active_piece_orientation_index]
+    ACTIVE_PIECE_INFO = pieces[active_piece_name]
+    ACTIVE_PIECE_ORIENTATION_INDEX = 0
+    ACTIVE_PIECE_ORIENTATION = ACTIVE_PIECE_INFO['orientations'][ACTIVE_PIECE_ORIENTATION_INDEX]
 
-    active_piece_col = int(squares_width / 2) - 2
-    active_piece_row = 0
+    ACTIVE_PIECE_COL = int(SQUARES_WIDTH / 2) - 2
+    ACTIVE_PIECE_ROW = 0
 
 
 def key_press(event):
-    global active_piece_col, active_piece_orientation, active_piece_orientation_index
+
+    """ Examine key press event from user, and decide what action to take """
+    global ACTIVE_PIECE_COL, ACTIVE_PIECE_ORIENTATION, ACTIVE_PIECE_ORIENTATION_INDEX
 
     key = event.keysym   # the letter, or 'Up', 'Down', 'Left', 'Right' ... 
     if key == 'Left':
-        if active_piece_col > 0:
-            if can_move_piece(active_piece_row, active_piece_col - 1):
-                active_piece_col -= 1
+        if ACTIVE_PIECE_COL > 0:
+            if can_move_piece(ACTIVE_PIECE_ROW, ACTIVE_PIECE_COL - 1):
+                ACTIVE_PIECE_COL -= 1
     
     elif key == 'Right':
-        active_piece_width = len(active_piece_orientation.split('\n')[0])
-        if active_piece_col + active_piece_width < squares_width:   # if not bump into wall...
-            if can_move_piece(active_piece_row, active_piece_col + 1):   # or other set pieces
-                active_piece_col += 1   
+        active_piece_width = len(ACTIVE_PIECE_ORIENTATION.split('\n')[0])
+        if ACTIVE_PIECE_COL + active_piece_width < SQUARES_WIDTH:   # if not bump into wall...
+            if can_move_piece(ACTIVE_PIECE_ROW, ACTIVE_PIECE_COL + 1):   # or other set pieces
+                ACTIVE_PIECE_COL += 1   
     
     elif key == 'Up':  # rotate 
-        next_index = (active_piece_orientation_index + 1) % len(active_piece_info['orientations'])
-        next_orientation = active_piece_info['orientations'][next_index]
+        next_index = (ACTIVE_PIECE_ORIENTATION_INDEX + 1) % len(ACTIVE_PIECE_INFO['orientations'])
+        next_orientation = ACTIVE_PIECE_INFO['orientations'][next_index]
 
-        if can_move_piece(active_piece_row, active_piece_col, next_orientation):
-            active_piece_orientation_index = (active_piece_orientation_index + 1) % len(active_piece_info['orientations'])
-            active_piece_orientation = active_piece_info['orientations'][active_piece_orientation_index]
+        if can_move_piece(ACTIVE_PIECE_ROW, ACTIVE_PIECE_COL, next_orientation):
+            ACTIVE_PIECE_ORIENTATION_INDEX = (ACTIVE_PIECE_ORIENTATION_INDEX + 1) % len(ACTIVE_PIECE_INFO['orientations'])
+            ACTIVE_PIECE_ORIENTATION = ACTIVE_PIECE_INFO['orientations'][ACTIVE_PIECE_ORIENTATION_INDEX]
     
-    elif key == 'r':
+    elif key == 'r':  # If the R key is pressed, restart the game 
         initialize_game()
 
 
 def move_active_piece():
-    global active_piece_row
-    if can_move_piece(active_piece_row + 1, active_piece_col):
-        active_piece_row += 1
+    """ Move the active piece down one row if possible.
+    Return true if the piece can be moved down """
+    global ACTIVE_PIECE_ROW
+    if can_move_piece(ACTIVE_PIECE_ROW + 1, ACTIVE_PIECE_COL):
+        ACTIVE_PIECE_ROW += 1
         return True
     else:
         set_piece()
         return False 
 
 
-def set_piece():
-    component_squares = active_piece_orientation.split('\n')
+def set_piece() :
+    component_squares = ACTIVE_PIECE_ORIENTATION.split('\n')
     for row_index, line in enumerate(component_squares):
         for col_index, square in enumerate(line):
             if square == '*':  # this is a part of the active piece
-                row_loc = active_piece_row + row_index
-                col_loc = active_piece_col + col_index
-                grid[row_loc][col_loc] = active_piece_info['color']
+                row_loc = ACTIVE_PIECE_ROW + row_index
+                col_loc = ACTIVE_PIECE_COL + col_index
+                grid[row_loc][col_loc] = ACTIVE_PIECE_INFO['color']
 
 
 def can_move_piece(next_row, next_col, next_orientation=None):
@@ -152,7 +161,7 @@ def can_move_piece(next_row, next_col, next_orientation=None):
     if next_orientation:
         orientation = next_orientation
     else:
-        orientation = active_piece_orientation
+        orientation = ACTIVE_PIECE_ORIENTATION
 
     piece_height = len(orientation.split('\n'))
     
@@ -192,11 +201,11 @@ def remove_complete_rows():
 
 def game_over():
     # A new piece added at the start, if it can't move, then the board must be full. 
-    return not can_move_piece(0, int(squares_width / 2))
+    return not can_move_piece(0, int(SQUARES_WIDTH / 2))
 
 
 def clear():
-    w.create_rectangle(0, 0, canvas_width, canvas_height, fill=background)
+    w.create_rectangle(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, fill=BACKGROUND_COLOR)
 
 
 def draw_all(show_active_peice=True):
@@ -212,37 +221,37 @@ def draw_current_pieces():
     for row_index, row in enumerate(grid):
         for col_index, col in enumerate(row):
             if col:
-                x = col_index * square_side
-                y = row_index * square_side
-                w.create_rectangle(x, y, x + square_side, y + square_side, fill=col)
+                x = col_index * SQUARE_SIDE
+                y = row_index * SQUARE_SIDE
+                w.create_rectangle(x, y, x + SQUARE_SIDE, y + SQUARE_SIDE, fill=col)
 
 
 def draw_active_piece(): 
-    component_squares = active_piece_orientation.split('\n')
+    component_squares = ACTIVE_PIECE_ORIENTATION.split('\n')
 
     for row_index, line in enumerate(component_squares):
         for col_index, square in enumerate(line):
             if square == '*':
-                x = (active_piece_col + col_index) * square_side
-                y = (active_piece_row + row_index) * square_side
-                w.create_rectangle(x, y, x+square_side, y+square_side, fill=active_piece_info['color'])
+                x = (ACTIVE_PIECE_COL + col_index) * SQUARE_SIDE
+                y = (ACTIVE_PIECE_ROW + row_index) * SQUARE_SIDE
+                w.create_rectangle(x, y, x+SQUARE_SIDE, y+SQUARE_SIDE, fill=ACTIVE_PIECE_INFO['color'])
 
 
 def draw_grid():
     # horizontal lines
-    for index, row in enumerate(grid):
-        w.create_line(0, index*square_side, canvas_width, index*square_side, fill=grid_line_color)  # x1, y1, x2, y2
+    for index, _ in enumerate(grid):
+        w.create_line(0, index*SQUARE_SIDE, CANVAS_WIDTH, index*SQUARE_SIDE, fill=GRID_LINE_COLOR)  # x1, y1, x2, y2
 
     # vertical lines 
-    for index, col in enumerate(grid[0]):
-        w.create_line(index*square_side, 0, index*square_side, canvas_height, fill=grid_line_color)
+    for index, _ in enumerate(grid[0]):
+        w.create_line(index*SQUARE_SIDE, 0, index*SQUARE_SIDE, CANVAS_HEIGHT, fill=GRID_LINE_COLOR)
 
 
 def game_loop():   
     
     if game_over():
-        w.create_rectangle(0, canvas_height / 3, canvas_height, canvas_height / 3 * 2, fill='gray')
-        w.create_text(int(canvas_width/2), int(canvas_height/2), text='Game Over\nPress R to restart', font="Courier 20 bold", fill='deeppink')
+        w.create_rectangle(0, CANVAS_HEIGHT / 3, CANVAS_HEIGHT, CANVAS_HEIGHT / 3 * 2, fill='gray')
+        w.create_text(int(CANVAS_WIDTH/2), int(CANVAS_HEIGHT/2), text='Game Over\nPress R to restart', font="Courier 20 bold", fill='deeppink')
 
     else:
         was_moved_down = move_active_piece()
@@ -255,7 +264,8 @@ def game_loop():
             start_new_piece()
         
 
-    root.after(speed, game_loop)
+    root.after(speed, game_loop)   
+  
 
         
 root.bind('<Key>', key_press)  # key event handlers
